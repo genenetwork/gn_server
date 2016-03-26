555a8d9e773e270d18c56efb5244c3747f044d6c
Pjotr Prins <pjotr.public01@thebird.nl>
2016-03-26 11:05:58 -0500

    Testing MySQL

--- lib/gn_server.ex
+++ lib/gn_server.ex
@@ -1,9 +1,20 @@
 
 defmodule GnServer.Router.Homepage do
   use Maru.Router
-  
+
   get "/hey" do
-    json(conn, %{hello: :hey})
+    {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "localhost")
+    {:ok, result} = Mysqlex.Connection.query(pid, "SELECT title FROM posts", [])
+    rec = Map.from_struct(result)
+    IO.puts :stderr, "HERE"
+    lines = rec[:rows]
+    for item <- lines do
+      {s} = item
+      IO.puts s
+    end
+    IO.inspect(Enum.join(lines))
+    # Enum.join(tuple_to_list(lines))
+    json(conn, lines.first)
   end
   get do
       json(conn, %{hello: :world})
@@ -13,10 +24,20 @@ end
 
 defmodule GnServer.API do
   use Maru.Router
-
-  {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "127.0.0.1")
   
   mount GnServer.Router.Homepage
+  IO.puts "HELLO"
+
+    {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "localhost")
+
+    {:ok, result} = Mysqlex.Connection.query(pid, "SELECT title FROM posts", [])
+    rec = Map.from_struct(result)
+    lines = rec[:rows]
+    for item <- lines do
+      {s} = item
+      IO.puts s
+    end
+    IO.inspect(lines)
 
   rescue_from :all do
     conn
