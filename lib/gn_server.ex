6b68c6de4f83db512096d55f7364fbfb1c1deed4
Pjotr Prins <pjotr.public01@thebird.nl>
2016-03-26 08:53:02 -0500

    REST works and adding MySQLex

--- lib/gn_server.ex
+++ lib/gn_server.ex
@@ -1,23 +1,26 @@
 
 defmodule GnServer.Router.Homepage do
-    use Maru.Router
-
-    get "/hey" do
-      json(conn, %{hello: :hey})
-    end
-    get do
+  use Maru.Router
+  
+  get "/hey" do
+    json(conn, %{hello: :hey})
+  end
+  get do
       json(conn, %{hello: :world})
-    end
+  end
+
 end
 
 defmodule GnServer.API do
-    use Maru.Router
+  use Maru.Router
 
-    mount GnServer.Router.Homepage
+  {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "127.0.0.1")
+  
+  mount GnServer.Router.Homepage
 
-    rescue_from :all do
-      conn
-      |> put_status(500)
-      |> text("*** Server Error")
-    end
+  rescue_from :all do
+    conn
+    |> put_status(500)
+    |> text("*** Server Error")
+  end
 end
