8be881bf21e8353c8096c68ec5a1952088b25d30
Pjotr Prins <pjotr.public01@thebird.nl>
2016-03-27 16:28:13 -0500

    Unpack struct directly

--- lib/gn_server.ex
+++ lib/gn_server.ex
@@ -5,9 +5,11 @@ defmodule GnServer.Router.Homepage do
   get "/hey" do
     {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "localhost")
     {:ok, result} = Mysqlex.Connection.query(pid, "SELECT title FROM posts", [])
-    rec = Map.from_struct(result)
-    lines = rec[:rows]
-    nlist = Enum.map(lines, fn(x) -> {s} = x ; s end)
+    # rec = Map.from_struct(result)
+    # lines = rec[:rows]
+    %Mysqlex.Result{rows: rows} = result
+    # IO.inspect(rows)
+    nlist = Enum.map(rows, fn(x) -> {s} = x ; s end)
     # IO.puts Poison.encode_to_iodata!(nlist)
     json(conn, nlist)
   end
@@ -21,19 +23,7 @@ defmodule GnServer.API do
   use Maru.Router
   
   mount GnServer.Router.Homepage
-  IO.puts "HELLO"
-
-    {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "localhost")
-
-    {:ok, result} = Mysqlex.Connection.query(pid, "SELECT title FROM posts", [])
-    rec = Map.from_struct(result)
-    lines = rec[:rows]
-    for item <- lines do
-      {s} = item
-      IO.puts s
-    end
-    IO.inspect(lines)
-
+  IO.puts "Starting server"
   rescue_from :all do
     conn
     |> put_status(500)
--- test/mysql_test.exs
+++ test/mysql_test.exs
@@ -2,22 +2,13 @@ defmodule MySQLTest do
   use ExUnit.Case
   # doctest GnServer
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-  test "the truth2" do
+  test "Test MySQL connection" do
     {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "localhost")
     {:ok, result} = Mysqlex.Connection.query(pid, "SELECT title FROM posts", [])
-    rec = Map.from_struct(result)
-    IO.puts :stderr, "HERE"
-    lines = rec[:rows]
-    for item <- lines do
-      {s} = item
-      IO.puts s
-    end
-    # IO.inspect(lines)
-    # json(pid, lines)
-    nlist = Enum.map(lines, fn(x) -> {s} = x ; s end)
+    # rec = Map.from_struct(result)
+    %Mysqlex.Result{rows: rows} = result
+    IO.inspect(rows)
+    nlist = Enum.map(rows, fn(x) -> {s} = x ; s end)
     IO.puts Poison.encode_to_iodata!(nlist)
     true
   end
