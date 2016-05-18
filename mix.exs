dea2c5f5dd6cfc4728ba71683fcd0c4a0c7acdeb
Pjotr Prins <pjotr.public01@thebird.nl>
2016-05-18 10:10:23 +0000

    Update maru version

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule GnServer.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   def deps do
-  [ {:maru, "~> 0.8"} ,
+  [ {:maru, "~> 0.9.5"} ,
     {:mysqlex, github: "tjheeta/mysqlex" } ]  
   end
 end
