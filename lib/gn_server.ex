d180320ac10f5a6c34a2cc937fc5298b12057d1b
Pjotr Prins <pjotr.public01@thebird.nl>
2016-05-25 08:22:14 +0000

    /species: Fetching species info

--- lib/gn_server.ex
+++ lib/gn_server.ex
@@ -2,6 +2,32 @@
 defmodule GnServer.Router.Homepage do
   use Maru.Router
 
+  # DB_URI = "mysql://gn2:mysql_password@localhost/db_webqtl_s"
+  get "/species" do
+    {:ok, pid} = Mysqlex.Connection.start_link(username: "gn2", database: "db_webqtl_s", password: "mysql_password", hostname: "localhost")
+    {:ok, result} = Mysqlex.Connection.query(pid, "SELECT * FROM Species", [])
+    # rec = Map.from_struct(result)
+    %Mysqlex.Result{rows: rows} = result
+    # IO.inspect(rows)
+    nlist = Enum.map(rows, fn(x) -> {_,species_id,species_name,_,_,full_name,_,_} = x ; [species_id,species_name,full_name] end)
+    IO.puts Poison.encode_to_iodata!(nlist)
+    IO.puts Enum.join(nlist,"\n")
+    json(conn, nlist)
+  end
+
+  get "/mouse" do
+    # Fetch all crosses from InBredSet and Strain
+    {:ok, pid} = Mysqlex.Connection.start_link(username: "gn2", database: "db_webqtl_s", password: "mysql_password", hostname: "localhost")
+    {:ok, result} = Mysqlex.Connection.query(pid, "SELECT * FROM Species", [])
+    # rec = Map.from_struct(result)
+    %Mysqlex.Result{rows: rows} = result
+    # IO.inspect(rows)
+    nlist = Enum.map(rows, fn(x) -> {_,species_id,species_name,_,_,full_name,_,_} = x ; [species_id,species_name,full_name] end)
+    IO.puts Poison.encode_to_iodata!(nlist)
+    IO.puts Enum.join(nlist,"\n")
+    json(conn, nlist)
+  end
+
   get "/hey2" do
     {:ok, pid} = Mysqlex.Connection.start_link(username: "test", database: "test", password: "test", hostname: "localhost")
     {:ok, result} = Mysqlex.Connection.query(pid, "SELECT title FROM posts", [])
