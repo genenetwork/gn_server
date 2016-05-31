adb5c7d89d39d08dea7eb7b3420f3dea6eddda75
christian <christian@chfi.se>
2016-05-31 14:07:15 -0400

    Serve R/QTL2 CSV & JSON sample data

--- lib/gn_server.ex
+++ lib/gn_server.ex
@@ -25,10 +25,31 @@ defmodule GnServer.Router.Homepage do
 
 end
 
+defmodule GnServer.Router.Rqtl do
+
+  use Maru.Router
+
+  get "/iron.json" do
+    {:ok, content} = File.read("./iron.json")
+    text(conn, content)
+  end
+
+  get "/iron_gmap.csv" do
+    {:ok, content} = File.read("./iron_gmap.csv")
+    text(conn, content)
+  end
+
+  get "/iron_geno.csv" do
+    {:ok, content} = File.read("./iron_geno.csv")
+    text(conn, content)
+  end
+end
+
 defmodule GnServer.API do
   use Maru.Router
 
   mount GnServer.Router.Homepage
+  mount GnServer.Router.Rqtl
 
   IO.puts "Starting server"
   
