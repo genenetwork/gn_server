16dc4a3ae161fb62ef1a07d2bf7540af53f174cd
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-14 10:49:07 +0000

    Building up the GN menu using GN_SERVER

--- lib/api/internal.ex
+++ lib/api/internal.ex
@@ -1,13 +1,18 @@
 defmodule GnServer.Router.IntAPI do
 
   use Maru.Router
-
+  
   IO.puts "Setup routing"
 
   alias GnServer.Data.Store, as: Store
 
+
   get "int/" do
     json(conn, %{"I am": :genenetwork, api: :internal})
+  end
+  
+  get "int/menu/species" do
+    json(conn, Store.menu_species)
   end
 
 end
--- test/api/internal_test.exs
+++ test/api/internal_test.exs
@@ -11,4 +11,9 @@ defmodule InternalAPITest do
     assert state == value
   end
 
+  test "int/menu/species", %{hello: state} do
+    %Plug.Conn{resp_body: value} = conn(:get, "int/menu/species") |> make_response
+    assert value == Poison.encode!([[1,"mouse","Mouse"],[4,"human","Human"]])
+  end
+
 end
