adc0698026dc16cd21ec506cbd57f812921a6f49
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-15 06:33:20 +0000

    Removing header info from general REST service

--- lib/gn_server.ex
+++ lib/gn_server.ex
@@ -1,15 +1,15 @@
 defmodule GnServer.API do
   use Maru.Router
   
-  plug CORSPlug, origin: ["*"], headers: ["Range", "If-None-Match", "Accept-Ranges"], expose: ["Content-Range"]
+  plug CORSPlug, origin: ["*"]
   plug Plug.Head
 
   mount GnServer.Router.MainAPI
   mount GnServer.Router.IntAPI
   mount GnServer.Router.Rqtl
   mount GnServer.Router.SNP
-  mount GnServer.Router.Stylesheets
-  mount GnServer.Router.QTL
+  # mount GnServer.Router.Stylesheets
+  # mount GnServer.Router.QTL
 
   IO.puts "Starting server"
   
