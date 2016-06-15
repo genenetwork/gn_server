d5ff80dc8647c653f8638e956ad3de2f966a4fa0
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-15 16:19:34 +0000

    Fetching tissue/type info

--- lib/logic/assemble.ex
+++ lib/logic/assemble.ex
@@ -1,20 +1,33 @@
 defmodule GnServer.Logic.Assemble do
 
   alias GnServer.Data.Store, as: Store
-  
+
   def menu_main do
     species = Store.menu_species
     nlist = Enum.map(species, fn(x) -> [_,sname,_]=x ;
         [sname,Store.menu_groups(sname)]
       end
     );
+    groups = for [k,v] <- nlist, into: %{}, do: { k, v }
 
-    IO.inspect "SHOW"
     IO.inspect nlist
-    hash = for [k,v] <- nlist, into: %{}, do: { k, v }
-     
+    list =
+      for [s,gs] <- nlist, # for every species,group combi
+        do: Enum.map(gs, fn(g) -> # for every group
+              [_,gname,_] = g
+              ts = Store.menu_types(s,gname) # fetch types
+              {s,gname,ts}
+            end)
+    # list contains a list of tuples {s,g,ts}
+    types = %{}
+    Enum.each(list, fn(tup) ->
+      IO.inspect "***"
+      IO.inspect(tup)
+    end)
+
     %{ species: species,
-       groups: hash}
+       groups: groups,
+       types: types }
   end
-  
+
 end
