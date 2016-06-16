7105f38bf438bfc6ae7dfa4f72646ba476ed4d2b
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-16 07:41:46 +0000

    Assemble: fix menu test - now passes

--- test/api/internal_test.exs
+++ test/api/internal_test.exs
@@ -14,13 +14,19 @@ defmodule InternalAPITest do
   test "int/menu/main" do
     %Plug.Conn{resp_body: value} = conn(:get, "int/menu/main.json") |> make_response
     assert Poison.decode!(value) ==
-      %{"species" => [[1,"mouse","Mouse"],[4,"human","Human"]],
-        "types" => %{"human" => %{"HLC" => [["Liver mRNA"]]},
-                     "mouse" => %{"BXD" => [["Hippocampus mRNA"]]}},
-        "groups" => %{ "mouse" =>
-                         [[1,"BXD","BXD"]],
-                       "human" =>
-                         [[34,"HLC","Liver: Normal Gene Expression with Genotypes (Merck)"]]}}
+      %{"datasets" => %{"human" => %{"HLC" => %{"Liver mRNA" => [[320,
+                     "HLC_0311",
+                     "GSE9588 Human Liver Normal (Mar11) Both Sexes"]]}},
+               "mouse" => %{"BXD" => %{"Hippocampus mRNA" => [[112,
+                     "HC_M2_0606_P",
+                     "Hippocampus Consortium M430v2 (Jun06) PDNN"]]}}},
+             "groups" => %{"human" => [[34, "HLC",
+                 "Liver: Normal Gene Expression with Genotypes (Merck)"]],
+               "mouse" => [[1, "BXD", "BXD"]]},
+             "species" => [[1, "mouse", "Mouse"], [4, "human", "Human"]],
+             "types" => %{"human" => %{"HLC" => [["Liver mRNA"]]},
+               "mouse" => %{"BXD" => [["Hippocampus mRNA"]]}}}
+
   end
 
 end
