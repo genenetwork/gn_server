fcae7b7f6ada13c4482685e6536a438decafc118
Pjotr Prins <pjotr.public01@thebird.nl>
2016-06-28 08:41:53 +0000

    fetch SNP info

--- test/api/main_test.exs
+++ test/api/main_test.exs
@@ -82,7 +82,9 @@ defmodule APITest do
   test "/genotype/mouse/marker/rs3693478.json" do
     %Plug.Conn{resp_body: value} = conn(:get, "/genotype/mouse/marker/rs3693478.json") |> make_response
     assert Poison.decode!(value) ==
-    [%{"symbol" => "null", "MAX_LRS" => 30.4944361132252, "Mb" => 12.6694, "chr" => 12, "mean" => 7.232, "name" => "1452452_at", "name_id" => 1452452, "p_value" => 6.09756097560421e-5, "additive" => 0.392331541218638, "locus" => "gnf12.013.284"}]
+     [%{"chr" => "7", "chr_len" => 67.179978, "marker" => "rs3693478", "source" => "Illumina_5530", "species" => "mouse", "species_id" => 1}]
+
+
   end
 
   # Should add a test here for non-public
