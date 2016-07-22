c033b4da515d02c9976240db18117d902ca16526
christian <christian@chfi.se>
2016-07-22 14:46:20 +0200

    snp_counts() now returns list of maps

--- lib/gn_server/biodalliance/snp_density.ex
+++ lib/gn_server/biodalliance/snp_density.ex
@@ -4,8 +4,16 @@ defmodule GnServer.Biodalliance.SNPDensity do
   def snp_counts(chr, start_mb, end_mb, step, strain_id1, strain_id2) do
     num_bins = round((end_mb - start_mb) / step)
 
-    0..num_bins
+    bins = 0..num_bins
+    |> Enum.map(fn b -> start_mb + (b * step) end)
+    |> Enum.map(fn b -> %{start: b, end: (b+step)} end)
+
+    counts = 0..num_bins
     |> Enum.map(fn b -> start_mb + (b * step) end)
     |> Enum.map(fn b -> Store.snp_count(chr, b, step, strain_id1, strain_id2) end)
+
+    result = Enum.zip(bins, counts)
+    |> Enum.map(fn {b, c} -> Map.put(b, :score, c) end)
+    result
   end
 end
