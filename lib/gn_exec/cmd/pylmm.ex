6e99e1e786347bf7a74e4e9446fbfdad84e6cfdc
Pjotr Prins <pjotr.public01@thebird.nl>
2016-09-12 14:45:26 +0200

    pylmm: capture output in cache

--- lib/gn_exec/cmd/pylmm.ex
+++ lib/gn_exec/cmd/pylmm.ex
@@ -1,12 +1,18 @@
 defmodule GnExec.Cmd.PyLMM do
   alias GnExec.Cmd
 
-  # First draft at running scanone on GnServer - this functionality
-  # will move to GnExec in the near future
+  # First draft at running pylmm on GnServer - this functionality
+  # will move to GnExec in the near future. pylmm
 
   def cmd(dataset) do
-    # File.write!("try.R",rscript)
-    {output, 0} = System.cmd "/home/wrk/izip/git/opensource/genenetwork/pylmm_gn2/bin/runlmm.py", ["--help"]
-    output
+    token = "8412ab517c6ef9c2f8b6dae3ed2a60cc"
+    cache_dir = Application.get_env(:gn_server, :cache_dir)
+    path = cache_dir <> "/" <> token
+    File.mkdir_p!(path)
+    # output = :os.cmd '/home/wrk/izip/git/opensource/genenetwork/pylmm_gn2/bin/runlmm.py --help'
+    {output, retval} = System.cmd "/home/wrk/izip/git/opensource/genenetwork/pylmm_gn2/bin/runlmm.py", ["--help"],
+      into: File.stream!(path <> "/STDOUT"), stderr_to_stdout: true
+    # File.write!(path <> "/STDOUT",output)  # We'll use System.cmd :into to track progress later
+    {retval,token}
   end
 end
